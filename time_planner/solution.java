2a96cae778acb7a91737d7da1e5297c4e2ad9313
Achyudh Ram <7617287+achyudh@users.noreply.github.com>
2019-05-13 18:28:51 -0400

    Create solution.java

---
+++ time_planner/solution.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+   
+  static int[] getOverlap(int[] slotA, int[] slotB) {
+    int[] overlap = new int[2];
+    overlap[0] = Math.max(slotA[0], slotB[0]);
+    overlap[1] = Math.min(slotA[1], slotB[1]);
+    
+    if (overlap[0] <= overlap[1])
+      // Return the overlapping interval
+      return overlap;
+    else
+      // In this case, there is no overlap
+      return new int[]{0, 0};
+  }
+  
+  static int[] meetingPlanner(int[][] slotsA, int[][] slotsB, int dur) {
+    int ptrA = 0, ptrB = 0;
+    
+    while (ptrA < slotsA.length && ptrB < slotsB.length) {
+      int[] overlap = getOverlap(slotsA[ptrA], slotsB[ptrB]);
+      
+      if (overlap[1] - overlap[0] >= dur)
+        // Return the first interval with the given duration
+        return new int[] {overlap[0], overlap[0] + dur};
+      
+      else {
+        // Increment the pointer for the earlier slot    
+        if (slotsA[ptrA][1] < slotsB[ptrB][1])
+          ptrA++;
+        else 
+          ptrB++;
+      }
+    }
+    
+    // No common time slot found
+    return new int[0];
+  }
+}
